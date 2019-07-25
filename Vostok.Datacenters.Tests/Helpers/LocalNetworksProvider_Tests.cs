dfa060fd50ec44b809e330a6030641847341e29c
kungurtsev <kungurtsev@skbkontur.ru>
2019-07-25 18:11:07 +0500

    try print

--- Vostok.Datacenters.Tests/Helpers/LocalNetworksProvider_Tests.cs
+++ Vostok.Datacenters.Tests/Helpers/LocalNetworksProvider_Tests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Net.NetworkInformation;
+using FluentAssertions;
 using NUnit.Framework;
 using Vostok.Datacenters.Helpers;
 
@@ -10,6 +12,12 @@ namespace Vostok.Datacenters.Tests.Helpers
         [Test]
         public void Should_locate_something()
         {
+            var x = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var networkInterface in x)
+            {
+                Console.WriteLine($"INTERFACE {networkInterface.Id} {networkInterface.OperationalStatus} {networkInterface.NetworkInterfaceType}");
+            }
+
             LocalNetworksProvider.Get().Should().NotBeEmpty();
         }
     }
