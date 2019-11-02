316444b800b7812149e30e8442c21f878b0b1e0d
iloktionov <iloktionov@skbkontur.ru>
2019-11-02 15:12:43 +0500

    + EmptyDatacenters

---
+++ Vostok.Datacenters/EmptyDatacenters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using JetBrains.Annotations;
+
+namespace Vostok.Datacenters
+{
+    [PublicAPI]
+    public class EmptyDatacenters : IDatacenters
+    {
+        public string GetLocalDatacenter() => null;
+
+        public string GetDatacenter(IPAddress address) => null;
+
+        public string GetDatacenter(string hostname) => null;
+
+        public IReadOnlyCollection<string> GetActiveDatacenters() => Array.Empty<string>();
+    }
+}
