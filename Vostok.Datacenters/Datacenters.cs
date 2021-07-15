62085bf886a306d58652e7a836270fe6d002fd9e
Татьяна Гладышева <gladysheva@skbkontur.ru>
2021-07-15 09:37:32 +0500

    Add logs about local dc.

--- Vostok.Datacenters/Datacenters.cs
+++ Vostok.Datacenters/Datacenters.cs
@@ -5,7 +5,9 @@ using System.Net;
 using JetBrains.Annotations;
 using Vostok.Commons.Environment;
 using Vostok.Commons.Helpers.Network;
+using Vostok.Commons.Threading;
 using Vostok.Datacenters.Helpers;
+using Vostok.Logging.Abstractions;
 
 namespace Vostok.Datacenters
 {
@@ -17,9 +19,12 @@ namespace Vostok.Datacenters
 
         private readonly DatacentersSettings settings;
         private readonly DnsResolver dnsResolver;
+        private readonly ILog log;
 
         private readonly string localDatacenter;
         private readonly string localHostname;
+
+        private bool isFirstLogging = new AtomicBoolean(true);
 
         public Datacenters([NotNull] DatacentersSettings settings)
         {
@@ -31,7 +36,11 @@ namespace Vostok.Datacenters
             localHostname = this.settings.LocalHostname
                             ?? EnvironmentInfo.FQDN;
 
+            LogInitialHostname();
+
             dnsResolver = new DnsResolver(settings.DnsCacheTtl, settings.DnsResolveTimeout);
+
+            log = settings.Log.ForContext<Datacenters>();
         }
 
         public string GetLocalDatacenter()
@@ -43,13 +52,25 @@ namespace Vostok.Datacenters
             {
                 var d = GetDatacenter(localHostname);
                 if (d != null)
+                {
+                    LogDcFromHostname(d);
+                    isFirstLogging = false;
                     return d;
+                }
             }
 
-            return LocalNetworksProvider
-                .Get()
-                .Select(GetDatacenter)
+            var ips = LocalNetworksProvider.Get();
+
+            LogIpList(ips);
+
+            var dc = ips.Select(GetDatacenter)
                 .FirstOrDefault(datacenter => datacenter != null);
+
+            LogDcFromIp(dc);
+
+            isFirstLogging = false;
+
+            return dc;
         }
 
         public string GetDatacenter(IPAddress address)
@@ -69,5 +90,28 @@ namespace Vostok.Datacenters
                 .Resolve(hostname, canWaitForDnsResolution)
                 .Select(GetDatacenter)
                 .FirstOrDefault(x => x != null);
+
+        private void LogInitialHostname()
+        {
+            log.Info("LocalHostname = '{localHostname}'", localHostname);
+        }
+
+        private void LogDcFromIp(string dc)
+        {
+            if (isFirstLogging)
+                log.Info("Dc from ip: '{dc}'", dc);
+        }
+
+        private void LogIpList(IPAddress[] ips)
+        {
+            if (isFirstLogging)
+                log.Info("Ip from network provider: '{ips}'", string.Join(",", ips.Select(x => x.ToString())));
+        }
+
+        private void LogDcFromHostname(string d)
+        {
+            if (isFirstLogging)
+                log.Info("Got dc from localHostname variable: '{d}'", d);
+        }
     }
 }
